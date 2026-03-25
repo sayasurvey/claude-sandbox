da007929ac3d5b05ec75a427073d9621e721ab25
sayasurvey <sayasurvey@yahoo.co.jp>
2026-03-26 01:55:53 +0900

    FIX: Nuxtの環境構築

---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    '@nuxt/eslint',
+  ],
+
+  typescript: {
+    strict: true,
+  },
+
+  devtools: { enabled: true },
+})
